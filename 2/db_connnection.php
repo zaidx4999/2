8eb7ffeff739770580b8f3cb67dcd20056e1501f
Zaid <zaid.hamoud@hotmail.com>
2021-09-28 10:59:26 +0200

    2

--- 2/db_connnection.php
+++ 2/db_connnection.php
@@ -4,7 +4,7 @@ function OpenCon()
  $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "";
- $db = "root2";
+ $db = "zaid";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
  return $conn;
