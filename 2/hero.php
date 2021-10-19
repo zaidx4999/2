eaa809c777343061c5be7383adfe6fb43782c2e2
Zaid <zaid.hamoud@hotmail.com>
2021-10-19 10:46:40 +0200

    3

--- 2/hero.php
+++ 2/hero.php
@@ -1,25 +1,27 @@
-<!-- Nav Bar Start -->
-<div class="navbar navbar-expand-lg bg-light navbar-light">
+<!-- Hero Start -->
+<div class="hero" id="home">
     <div class="container-fluid">
-        <a href="index.html" class="navbar-brand">DevFolio</a>
-        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-            <div class="navbar-nav ml-auto">
-                <a href="index.html" class="nav-item nav-link active">Home</a>
-                <a href="about.html" class="nav-item nav-link">About</a>
-                <a href="service.html" class="nav-item nav-link">Service</a>
-                <a href="experience.html" class="nav-item nav-link">Experience</a>
-                <a href="portfolio.html" class="nav-item nav-link">Portfolio</a>
-                <a href="price.html" class="nav-item nav-link">Price</a>
-                <a href="review.html" class="nav-item nav-link">Review</a>
-                <a href="team.html" class="nav-item nav-link">Team</a>
-                <a href="#blog" class="nav-item nav-link">Blog</a>
-                <a href="#contact" class="nav-item nav-link">Contact</a>
+        <div class="row align-items-center">
+            <div class="col-sm-12 col-md-6">
+                <div class="hero-content">
+                    <div class="hero-text">
+                        <p><?php echo $contact_title; ?></p>
+                        <h1></h1>
+                        <h2></h2>
+                        <div class="typed-text">Cool Features, Coffee Drinker, Awesome As F***, Shit Happens, Learn HTML Kids</div>
+                    </div>
+                    <div class="hero-btn">
+                        <a class="btn" href="">Try My Features</a>
+                        <a class="btn" href="">HTML Hero!</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 d-none d-md-block">
+                <div class="hero-image">
+                    <img src="img/android-logo.png" alt="Hero Image">
+                </div>
             </div>
         </div>
     </div>
 </div>
-<!-- Nav Bar End -->
+<!-- Hero End -->
